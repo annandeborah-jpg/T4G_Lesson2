bb5d6420caf1b16c9691579baf31c5a1896c9595
annandeborah <annandeborah@gmail.com>
2026-03-25 12:40:28 +0000

    src

---
+++ src/index.js
@@ -0,0 +1,22 @@
+console.log("Hello World! This is my first JavaScript code.");
+
+let x = 2;
+console.log(x);
+
+x= 5;
+console.log(x);
+
+let answer = prompt("What is your age now?");
+console.log(answer);
+
+answer = prompt("What is your age in 5 years?");
+console.log(answer);
+
+let num1 = 7;
+let num2 = 13;
+
+console.log(num1 + num2);
+console.log(num2 - num1);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num2 % num1);
