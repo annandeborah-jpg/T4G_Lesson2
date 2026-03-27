0aab9b5d12c85aa6d7048a14bedb95d9828d70c9
annandeborah <annandeborah@gmail.com>
2026-03-27 13:08:10 +0000

    add

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
-console.log("Hello World! This is my first JavaScript code.");
+// console.log("Hello World! This is my first JavaScript code.");
 
-let x = 2;
-console.log(x);
+// let x = 2;
+// console.log(x);
 
-x= 5;
-console.log(x);
+// x= 5;
+// console.log(x);
 
-let answer = prompt("What is your age now?");
-console.log(answer);
+// let answer = prompt("What is your age now?");
+// console.log(answer);
 
-answer = prompt("What is your age in 5 years?");
-console.log(answer);
+// answer = prompt("What is your age in 5 years?");
+// console.log(answer);
 
-let num1 = 7;
-let num2 = 13;
+// let num1 = 7;
+// let num2 = 13;
 
-console.log(num1 + num2);
-console.log(num2 - num1);
-console.log(num1 * num2);
-console.log(num1 / num2);
-console.log(num2 % num1);
+// console.log(num1 + num2);
+// console.log(num2 - num1);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num2 % num1);
+
+
+let number = 9; // creating a variiable and assigning a value
+if(number % 2 == 0) // check the condition in the bracket to see if its met 
+    {alert(`${number} is even`); // if condition is met then the variable is even and should be alerted
+    console.log(`${number} is even`) // if condition is met then its logged to the console
+} 
+else{
+    alert(`${number} is odd`); //if condition is not met then the varibale is odd and should not be alerted
+    console.log(`${number} is odd`) // if condition is not met then its not logged to the console
+}
